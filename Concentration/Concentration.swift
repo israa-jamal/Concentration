1b62e5bf598019856774fdad50993b75e679a453
Esraa Gamal <esraa.gamal98@gmail.com>
2020-09-08 15:39:01 +0200

    Improving

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -10,15 +10,22 @@ import Foundation
 
 class Concentration{
     
+    //array of struct card
     var cards = [Card]()
+    
+    //when you use concentration you have to init the number of pairs
     init(numOfPairOfCards : Int) {
-        for identifer in 0..<numOfPairOfCards{
-        let card = Card(identifer: identifer)
-        let matchingCard = card
+        //give every card a unique identifer
+        for i in 0..<numOfPairOfCards{
+            let card = Card(identifer: i)
+            //and copy this card
+            let matchingCard = card
+            //and add both matching cards to the array
             cards += [card, matchingCard]
-
+            
         }
     }
+    
     func chooseCard(at index: Int){
         
     }
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,51 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //create game
     lazy var game = Concentration(numOfPairOfCards: (cardButtons.count + 1) / 2)
     
-//    outlets
+    //    outlets
     @IBOutlet weak var flipLabel: UILabel!
     @IBOutlet var cardButtons: [UIButton]!
+    
     //array of emojis
+    let emojis = ["👾" ,"🧚🏻‍♂️" ,"🧞‍♂️" ,"🏎️" ,"👽" ,"🐲" ,"🤖" ,"🤺" ,"👻"]
+    func emoji (for card: Card) -> String{
+        return "?"
+    }
     var count = 0
     {
+        //whenever the count change the flipLabel change
         didSet{
             flipLabel.text = "Flips: \(count)"
-
         }
     }
-    let emojis = ["😡","😥","😍","😡","😍","😥"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-// what happens when you press on a card
+    
+    // what happens when you press on a card
     @IBAction func cardClicked(_ sender: UIButton) {
         count += 1
         if let card = cardButtons.firstIndex(of: sender){
             game.chooseCard(at: card)
             updateView()
         }
-       
     }
-//    what happens when you flip a card
-    func flipCard(with emoji : String, on button : UIButton){
-        if button.currentTitle != emoji
-        {
-            button.setTitle(emoji, for: .normal)
-            button.backgroundColor = #colorLiteral(red: 0.4142037239, green: 0.232510499, blue: 0.6825031726, alpha: 1)
-        }else{
-            button.setTitle("", for: .normal)
-            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)  }
-    }
+    
+    //update view when something changes
     func updateView(){
         for index in cardButtons.indices{
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            // if the card is not visable flip it up
             
+            if card.isDisplayed{
+                button.setTitle("", for: .normal)
+                button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            }else{
+                //when the card is visable flip it down
+                button.setTitle(emoji(for: card), for: .normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 0.4142037239, green: 0.232510499, blue: 0.6825031726, alpha: 0) : #colorLiteral(red: 0.4142037239, green: 0.232510499, blue: 0.6825031726, alpha: 1)
+            }
         }
     }
+    
+    
     
 }
 
